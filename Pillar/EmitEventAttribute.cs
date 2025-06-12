fa5c36d82384b12a4df4aa8cef23ad1c52ded37e
mingmoe <me@kawayi.moe>
2025-06-12 21:46:06 +0800

    Initial commit

---
+++ Pillar/EmitEventAttribute.cs
@@ -0,0 +1,6 @@
+namespace Pillar;
+
+[AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+public sealed class EmitEventAttribute : Attribute
+{
+}
---
+++ update_package_json.ps1
@@ -0,0 +1,2 @@
+
+dotnet list .\Pillar\Pillar.csproj package --include-transitive --format json --output-version 1 > packages.json
